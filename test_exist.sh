907d9f6c6737802cfeb205c732b3fce7bc5e3a1f
yqf111000 <57945634+yqf111000@users.noreply.github.com>
2019-12-11 17:16:10 +0800

    修改test_exist.sh文件
    将if条件中的文件路径改为绝对路径，因为此时是一个相对路径，也就是该项目的当前根路径

--- test_exist.sh
+++ test_exist.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [[ -e "./pipeline_script_from_SCM/" ]]; then
+if [[ -e "/var/lib/jenkins/workspace/pipeline_script_from_SCM" ]]; then
 	#statements
-	rm -rf pipeline_script_from_SCM*
+	rm -rf /var/lib/jenkins/workspace/pipeline_script_from_SCM*
 	echo "###########################################################" > ./console.txt
-	echo "############# rm -rf pipeline_script_from_SCM* ############" >> ./console.txt
+	echo "############# rm -rf /var/lib/jenkins/workspace/pipeline_script_from_SCM* ############" >> ./console.txt
 	echo "###########################################################" >> ./console.txt
 fi
