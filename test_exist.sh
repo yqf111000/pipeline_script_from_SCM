070f32cf35a51f748522c962f24f3f9912f8f130
yqf111000 <57945634+yqf111000@users.noreply.github.com>
2019-12-11 17:29:57 +0800

    修改 test_exist.sh内容
    改为输出一句话

--- test_exist.sh
+++ test_exist.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-if [[ -e "../pipeline_script_from_SCM" ]]; then
-	#statements
-	rm -rf ../pipeline_script_from_SCM*
-	echo "######################################################################################" > ./console.txt
-	echo "############# rm -rf /var/lib/jenkins/workspace/pipeline_script_from_SCM* ############" >> ./console.txt
-	echo "######################################################################################" >> ./console.txt
-fi
+echo "脚本已经被执行了" > console.txt
